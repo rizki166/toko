6cd4af840ccac08844761630ace54425eea10158
rizki166 <rizkimuhamad953@gmail.com>
2023-11-16 20:58:22 +0700

    ajax, asynchronous, promises, async await,

--- day 6/JS/testimonials.js
+++ day 6/JS/testimonials.js
@@ -17,7 +17,7 @@ class Testimonial {
 }
 
 const testimonials = [
-    new Testimonial("Surya Elidanto", "Mantap sekali jasanya!", "https://images.pexels.com/photos/19040835/pexels-photo-19040835/free-photo-of-pria-jalan-jaket-potret.jpeg?auto=compress&cs=tinysrgb&w=600"),
+    new Testimonial("Surya Elidanto", "Mantap sekali jasanya!", "https://images.pexels.com/photos/19040835/pexels-photo-19040835/free-photo-of-pria-jalan-jaket-potret.jpeg?auto=compress&cs=tinysrgb&w=600gi"),
     new Testimonial("Surya elz", "Keren lah pokonya!", "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&w=600"),
     new Testimonial("Ujang karbu", "Wuhuu Keren lah!", "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600")
 ];
--- day 8/JS/testimonials.js
+++ day 8/JS/testimonials.js
@@ -26,35 +26,35 @@
 // testimonials.forEach(testimonial => {
 //     testimonialsContainer.innerHTML += testimonial.html();
 // });
-// JS/testimonials.js
 
-// Contoh data testimoni (gantilah dengan data yang sesuai)
+// HOF 
+
 const testimonialData = {
-  naufal: {
-    author: "Naufal",
+  Suryaelidanto: {
+    author: "Surya Elidanto",
     content: "Keren banget jasanya!",
     image: "https://images.pexels.com/photos/19040835/pexels-photo-19040835/free-photo-of-pria-jalan-jaket-potret.jpeg?auto=compress&cs=tinysrgb&w=600",
     rating: 5,
   },
-  novri: {
-    author: "Novri",
+  Suryaelz: {
+    author: "Surya Elz",
     content: "Keren lah pokonya!",
     image: "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&w=600",
     rating: 4,
   },
-  denis: {
-    author: "Denis",
+  Ujangkarbu: {
+    author: "Ujang Karbu",
     content: "the best pelayanannya!",
     image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
     rating: 4,
   },
-  febry: {
-    author: "Febry",
+  Suryaaa: {
+    author: "Suryaaa",
     content: "Oke lah!",
     image: "https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&w=600",
     rating: 3,
   },
-  anton: {
+  anton : {
     author: "anton",
     content: "apa apaan ini!",
     image: "https://images.pexels.com/photos/19038311/pexels-photo-19038311/free-photo-of-pria-trotoar-duduk-cerutu.jpeg?auto=compress&cs=tinysrgb&w=600",
